570e51554e726aec89bf68337079a9155962c52a
Seth Shikora <seth@shikora.org>
2020-06-17 19:53:49 -0700

    add dropdown demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PickerDemo from './components/picker/PickerDemo'
+import DropdownDemo from './components/dropdown/DropdownDemo'
 
 function App() {
   return (
     <div className="App">
       <PickerDemo>
       </PickerDemo>
+      <DropdownDemo>
+      </DropdownDemo>
     </div>
   );
 }
